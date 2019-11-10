563587d9a8dd5591627f64410166822076cb42b7
Marta Pancaldi <marta.panc@gmail.com>
2019-11-10 16:48:59 +0000

    Updates

---
+++ precache-manifest.a731db03895aefc6669ee6527928a5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d475f06424b273b49e48b2f7a7f0ad5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f5c61c9c97350435f961",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ba0a419edcca97483943",
+    "url": "/react-gh-pages/static/js/2.0ebdce7c.chunk.js"
+  },
+  {
+    "revision": "f5c61c9c97350435f961",
+    "url": "/react-gh-pages/static/js/main.8f946fa6.chunk.js"
+  },
+  {
+    "revision": "30423586187005e3020e",
+    "url": "/react-gh-pages/static/js/runtime-main.392e532c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
