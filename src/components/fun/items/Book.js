a6e15793c16acede052f19b061c2c964df3a19c3
Marta Pancaldi <marta.panc@gmail.com>
2020-01-12 23:14:39 +0000

    Add picts and text to fun section

---
+++ src/components/fun/items/Book.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+    constructor(props) {
+        super(props);
+
+        this.data = this.props.props;
+    }
+
+    render() {
+        return (
+            <div>
+                <a
+                    href={this.data.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        className="book-icon"
+                        src={this.data.cover}
+                        alt={this.data.title}
+                        height="70"
+                    />
+                </a>
+            </div>
+        );
+    }
+}
+
+export default Book;
--- src/components/home/Fun.js
+++ src/components/home/Fun.js
@@ -4,6 +4,9 @@ import book from '../../assets/images/fun/openBook.svg';
 import camera from '../../assets/images/fun/photo-camera.svg';
 import gamepad from '../../assets/images/fun/game-console.svg';
 import music from '../../assets/images/fun/headphones.svg';
+import BookSection from '../fun/BookSection';
+import games from '../../assets/images/games/gameShowcase.webp';
+import ig from '../../assets/images/insta.png';
 
 function Fun() {
     return (
@@ -29,19 +32,134 @@ function Fun() {
                     love doing in my free time :)
                 </div>
                 <div className="row">
-                    <div className="col-12 col-md-6 col-lg-6 ">
-                        <h5>Music</h5>
+                    <div className="col-12 col-md-6 col-lg-6 p-2">
+                        <h3
+                            style={{
+                                fontFamily: 'Baskerville',
+                                color: '#c11424',
+                            }}
+                        >
+                            Books
+                        </h3>
+                        <BookSection />
                     </div>
                     <div className="col-12 col-md-6 col-lg-6 ">
-                        <h5>Books</h5>
+                        <h3
+                            style={{
+                                fontFamily: 'Trebuchet MS',
+                                color: '#da8314',
+                            }}
+                        >
+                            Music
+                        </h3>
+                        <ul>
+                            <li>
+                                I studied the oboe for 12+ years, and although
+                                I've never planned to become a professional
+                                musician, I still practice it whenever I can.
+                                <br />
+                                Wait - you don't know what an oboe is? Check out
+                                this cool{' '}
+                                <a
+                                    href="https://drive.google.com/open?id=0Bz6vvbUH6o-_cl9pdTN3YmZlZlE"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    presentation{' '}
+                                </a>{' '}
+                                I prepared for a uni course! Also,{' '}
+                                <a
+                                    href="https://drive.google.com/open?id=1gtdxGuYPllZL9aMF6DXTrohCoOL1Y_Jj"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    1
+                                </a>
+                                ,{' '}
+                                <a
+                                    href="https://drive.google.com/open?id=1lALS5c92C4oL_xmObO4S62DyqFSQoBNU"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    2
+                                </a>
+                                ,{' '}
+                                <a
+                                    href="https://drive.google.com/open?id=1qIJwMlTUQrdacBkAsIVdeUX-MAQL5e_U"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    3
+                                </a>
+                                ,{' '}
+                                <a
+                                    href="https://www.youtube.com/watch?v=PDQHUyEdLhw"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    4
+                                </a>{' '}
+                                and{' '}
+                                <a
+                                    href="https://www.youtube.com/watch?v=F_VM95pANBk&t=24s"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    5
+                                </a>{' '}
+                                samples.
+                            </li>
+                            <li>
+                                I often find wrong notes or rhythmic errors when
+                                I listen to music... not sure if people around
+                                me are more impressed or annoyed by that 😛
+                            </li>
+                            <li>
+                                Anyway, I love music, in fact I'm constantly
+                                listening to some. <br />
+                                Classical composers are a favourite, but my iPod
+                                contains all sort of genres. Shuffle mode
+                                usually gives me interesting combinations, such
+                                as Beethoven and Lynyrd Skynyrd, followed by
+                                Hans Zimmer and Queen - all normal.
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-12 col-md-6 col-lg-6 ">
-                        <h5>Gaming</h5>
+                        <h3
+                            style={{
+                                fontFamily: 'Lucida Console',
+                                color: '#2A6FB7',
+                            }}
+                        >
+                            Travels & Photos
+                        </h3>
+                        I <u>never</u> go anywhere without my trusty Nikon
+                        camera. <br />
+                        If money and time weren't an issue, I think I'd be
+                        constantly on the road exploring the world.
+                        <br />
+                        You may want to check out a few pictures I took so far,
+                        in the{' '}
+                        <a href="/photos" rel="noopener noreferrer">
+                            dedicated section
+                        </a>
+                        . <br />
+                        I'm also active on &nbsp;
+                        <a
+                            href="https://www.instagram.com/pancakemarta/"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={ig} height="40" />
+                        </a>
                     </div>
                     <div className="col-12 col-md-6 col-lg-6 ">
-                        <h5>Travels & Photos</h5>
+                        <h3 style={{ fontFamily: 'Impact', color: '#226d10' }}>
+                            Gaming
+                        </h3>
+                        <img src={games} alt="Favourite games" />
                     </div>
                 </div>
             </div>
