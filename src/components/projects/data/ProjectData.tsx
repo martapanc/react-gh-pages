ca4ef6aec20eb78cc3470fea22974e316ac1a433
Marta Pancaldi <marta.panc@gmail.com>
2020-10-11 13:54:19 +0100

    Fix typo in project data

--- src/components/projects/data/ProjectData.tsx
+++ src/components/projects/data/ProjectData.tsx
@@ -200,7 +200,7 @@ export const ProjectData = [
         repo: "https://github.com/martapanc/ItalianFiscalCodeCalculatorApp",
         tags: ["java", "kotlin", "uni", "android"],
         text:
-            "I started learning Android development recently and, in order to focus on the feature of the mobile world," +
+            "I started learning Android development recently and, in order to focus on the feature of the mobile world, " +
             "as well as on practising with Kotlin, I've been reusing the Java backend of the Fiscal Code calculator " +
             "I built in uni. Check out the result on the " +
             '<a href="https://play.google.com/store/apps/details?id=com.pancaldim.fiscalcode_app">Play Store</a>!',
