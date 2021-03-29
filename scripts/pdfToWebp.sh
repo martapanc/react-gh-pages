1fa4ad23c7002f7a4256862758c8eec81a170206
martapanc <marta.panc@gmail.com>
2021-03-29 20:49:08 +0100

    Update CV

--- scripts/pdfToWebp.sh
+++ scripts/pdfToWebp.sh
@@ -8,6 +8,8 @@ convert -verbose -density 600 ./*.pdf -quality 100 cv.webp
 
 
 ### USAGE ###
+# The command 'convert' is part of the imageMagick library - install it locally with brew install imageMagick
+
 # Edit CV on Word and save it as PDF (name can have spaces or dashes)
 # Put PDF in the src/assets/images/cv folder
 # Run the script from the main directory
