83d2dfb0f0c5094d3e384308e8a9e38d242d6420
martapanc <marta.panc@gmail.com>
2021-11-03 21:19:21 +0000

    Add section structure

---
+++ src/components/fun/CoolFacts.tsx
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import {CoolFactsData, ComingSoon} from "./data/CoolFactsData";
+import $ from "jquery";
+import "./styles.scss";
+
+
+export default class CoolFacts extends Component {
+
+    componentDidMount() {
+        setTimeout(randomFact, 1000);
+    }
+
+    render() {
+        return (
+            <section className="overview-section p3 p-lg-3 responsive-adjust">
+                <div className="container cool-facts-container">
+                    <h2 className="section-title font-weight-bold mb-3 cool-facts-title">Cool facts about me</h2>
+                    <div className="cool-facts-item cool-facts-item__text">
+                        <p id="cool-fact" />
+                    </div>
+                    <div className="cool-facts-item cool-facts-item__button">
+                        <button id="cool-facts-button" className="btn btn-primary2" onClick={randomFact}>Get another random fact</button>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+function randomFact() {
+
+    if (CoolFactsData.length > 0) {
+        const index = Math.floor(Math.random() * CoolFactsData.length);
+        const factToShow = CoolFactsData[index]
+        $('#cool-fact').text(factToShow['text']);
+        CoolFactsData.splice(index)
+    } else {
+        $('#cool-fact').text(ComingSoon);
+        setTimeout(() => {
+            $('#cool-facts-button').fadeOut(2000, "linear");
+        }, 10);
+
+    }
+}
+
---
+++ src/components/fun/data/CoolFactsData.tsx
@@ -0,0 +1,18 @@
+export const CoolFactsData = [
+    {
+        title: "Peppuccio",
+        text: "I was an extra-actor in a movie once 🎬 It was \"The Correspondence\" (2016) by Giuseppe Tornatore, and " +
+            "I was part of the university students who sat in the conference hall and walked around the uni campus. If you " +
+            "manage to spot me, I promise I'll autograph your t-shirt ;)",
+    },
+    {
+        title: "Oboe",
+        text: "When I studied the oboe at the music school, I occasionally played for some illustrious people - notably, " +
+            "the ex President of the Republic Giorgio Napolitano, the Nobel prize Dario Fo, the bishop of my hometown, " +
+            "and the singer Luciano Ligabue 🎵 🎸 🎶 🎻"
+    }
+]
+
+export const ComingSoon = "That was the last cool fact I could think of... come back in some time for more :)";
+
+export default CoolFactsData;
